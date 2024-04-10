ba381fc6dc39f67c9a9b7179ff7a9fa0009bc9f3
cosmicsarthak <sarthakmohanty200@gmail.com>
2024-04-10 21:11:50 +0530

    s

--- app/page.jsx
+++ app/page.jsx
@@ -117,7 +117,7 @@ export default function Home() {
                   </div>
                 </section>
 
-                {/*  */}
+                {/* . */}
                 {/*  */}
                 {/*  */}
                 {/*  */}
